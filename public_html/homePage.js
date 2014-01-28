d41f9aea708454970790fcb82f741e4083e67089
Cameron Givler <cgiv812@gmail.com>
2014-01-28 12:30:21 -0500

    added homePage

---
+++ public_html/homePage.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function(){
+    embedpano({swf: "krpano/21_15_36_01_2nd.swf", xml: "krpano/21_15_36_01_2nd.xml", target: "pano", html5: "prefer", passQueryParameters: true});
+});
+
