b8c2997a5094e6a0874490e9b5f4729aff26effe
Cameron Givler <cgiv812@gmail.com>
2014-01-28 14:32:08 -0500

    Added a homepage template

--- public_html/homePage.js
+++ public_html/homePage.js
@@ -4,7 +4,19 @@
  * and open the template in the editor.
  */
 
-$(function(){
+$(function() {
     embedpano({swf: "krpano/21_15_36_01_2nd.swf", xml: "krpano/21_15_36_01_2nd.xml", target: "pano", html5: "prefer", passQueryParameters: true});
+    $('#logo').mouseover(function() {
+        $('.dropDown').css('z-index', '0');
+        $('#logoDrop').css('z-index', '1');
+    });
+    $('#about').mouseover(function() {
+        $('.dropDown').css('z-index', '0');
+        $('#aboutDrop').css('z-index', '1');
+    });
+    $('#images').mouseover(function() {
+        $('.dropDown').css('z-index', '0');
+        $('#imagesDrop').css('z-index', '1');
+    });
 });
 
